85f3dccbf4241b9b7639abdcfed36e95205a50ba
chef-nic <nicholasajohnson@me.com>
2024-07-11 11:46:22 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Nicholas Johnson on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
